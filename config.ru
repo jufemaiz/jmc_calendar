b94a86fe0fa420eef2c14182a6a101810d4f0dae
Joel Courtney <euphemize@gmail.com>
2010-12-07 12:15:17 +1100

    Calendar working

---
+++ config.ru
@@ -0,0 +1,2 @@
+require 'jmc_calendar'
+run Sinatra::Application
---
+++ jmc_calendar.rb
@@ -0,0 +1,25 @@
+require 'rubygems'
+require 'sinatra'
+require 'open-uri'
+require 'json'
+
+require 'haml'
+  set :haml, :format => :html5
+require 'sass'
+require 'coffee-script'
+
+# ----------------------------------
+# Index - default year is 2011
+# ----------------------------------
+get '/' do
+  @year = 2011
+  haml :index
+end
+
+# ----------------------------------
+# SCSS Custom Styling
+# ----------------------------------
+
+get '/css/styles.css' do
+  sass :styles
+end
