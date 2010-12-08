000b2a8aca39eaa755f85cc270bd8adc3d4030e0
Joel Courtney <euphemize@gmail.com>
2010-12-08 12:12:02 +1100

    Current and year selection now possible

--- jmc_calendar.rb
+++ jmc_calendar.rb
@@ -13,13 +13,24 @@ Linguistics::use( :en )
 # Index - default year is 2011
 # ----------------------------------
 get '/' do
-  @year = 2011
+  @today = Date.today
+  @year = @today.year
+  @start = @today
+  # Manage start and end of calendar
+  # Start
+  if @today.month == 1
+    @start = Date.new(@today.year - 1,12,1)
+  else
+    @start = Date.new(@today.year,@today.month-1,1)
+  end
+  @end = @start>>(12)
+  
   haml :index
 end
 
 get %r{/(\d+)$} do
   @year = params[:captures][0].to_i
-  haml :index
+  haml :annual
 end
 
 # ----------------------------------
